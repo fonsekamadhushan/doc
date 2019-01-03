dc1edadc85f6955fc5a71acb4314083f9812ae6b
Madhushan FONSEKA <46317190+fonsekamadhushan@users.noreply.github.com>
2019-01-03 09:47:10 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+// this is really basic of course - could use some refining no doubt.
+$("select").on("change click", "option, optgroup", function(e) {
+  var $this = $(this), $opts;
+
+  if ($this.is("option")) {
+    e.stopPropagation();
+    return;
+  }
+  else {
+    $opts = $this.find("option");
+    if (e.ctrlKey) {
+      //need to inverse the current selection, let's check the first el.
+	  if ($opts.first().attr("selected") !== undefined) {
+		$opts.removeAttr("selected");
+		$this.closest("select").trigger("blur"); //need to trigger this so the list actually updates.
+	  }
+	  else {
+	    $opts.attr("selected", "selected");
+	  }
+	  return;
+    }
+	else {
+	    $opts.attr("selected", "selected");
+	}
+  }
+});
